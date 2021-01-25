2c9cf4f58f72a9c8c76d531ea5560f021c3637bf
Bophelo <mpilo@vivaldi.net>
2021-01-25 16:51:15 +0200

    Protecting payment listings with a token

--- app/Controllers/PaymentsIndexController.php
+++ app/Controllers/PaymentsIndexController.php
@@ -7,11 +7,23 @@ use App\Models\Payment;
 use App\Controllers\Controller;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Exception\HttpNotFoundException;
 
 class PaymentsIndexController extends Controller {
 
     public function __invoke(Request $request, Response $response)
     {
+        //check if token exists
+        //dump($request->getQueryParams());
+        if(!$token = $request->getQueryParams()['token'] ?? false){
+            throw new HttpNotFoundException($request);
+        }
+
+        //verify in db
+        if(!Payment::where('token',$token)->first()){
+            throw new HttpNotFoundException($request);
+        }
+        
         $payments = Payment::latest()->get();
 
         return $this->container->get('view')->render($response, 'payments.twig', [
